f540cf57bb773b2d4ddf53477363e50b6a683fb8
Donal McNamee <mcnameed@archway.local>
2015-10-17 15:34:42 +0100

    First Commit
    Adding the InfraRed code to start making our Burglar Alarm

---
+++ BurglarAlarm/BurglarAlarm/BurglarAlarm.ino
@@ -0,0 +1,21 @@
+#include<IRremote.h>
+int RECV_PIN=11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  irrecv.enableIRIn();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(irrecv.decode(&results)){
+     Serial.println(results.value, DEC);
+     irrecv.resume();
+  }
+}
