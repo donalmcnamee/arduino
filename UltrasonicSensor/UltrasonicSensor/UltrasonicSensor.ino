af7342486b5d5788ad768e3964b280f05ab256ba
Rosemary Casserly <Rosemary Casserly>
2016-11-10 19:24:45 +0000

    Adding the firs revision of the UltraSonicSensor project

---
+++ UltrasonicSensor/UltrasonicSensor/UltrasonicSensor.ino
@@ -0,0 +1,42 @@
+int trigger=7;
+int echo=6;
+long time=0;
+
+long dist=0;
+
+void setup() 
+{
+Serial.begin(9600);
+
+pinMode(trigger, OUTPUT);
+pinMode(echo, INPUT);
+  
+
+}
+
+void loop()
+{
+digitalWrite(trigger, LOW);
+delay(5);
+digitalWrite(trigger, HIGH);
+delay(10);
+digitalWrite(trigger, LOW);
+time=pulseIn(echo, HIGH);
+
+dist=(time/2)/29.1;
+
+if(dist>=500||dist<=0){
+Serial.println("Nomeasurement");
+}
+
+else
+
+{
+  Serial.print(dist);
+  Serial.println("cm");
+}
+
+delay(1000);
+
+}
+
