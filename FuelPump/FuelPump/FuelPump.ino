7703512e46f3b6b8c5b5138900728b30ce74438b
Donal McNamee <donal.mcnamee@archwayproducts.com>
2019-09-01 19:04:01 +0100

    Reversing the bytearray containing the UID

--- FuelPump/FuelPump/FuelPump.ino
+++ FuelPump/FuelPump/FuelPump.ino
@@ -12,6 +12,8 @@ int del = 1000;
 // Teensy 2.0 has an LED on port 11
 int ledpin = 11;
 
+char dataStrings[50] = {0};
+
 void setup() {
   Serial.begin(9600);
   pinMode(ledpin, OUTPUT);
@@ -30,10 +32,16 @@ void loop() {
     delay(del);
     return;
   } else {
+    
+    sprintf(dataStrings, "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",addr[7],addr[6],addr[5],addr[4],addr[3],addr[2],addr[1],addr[0]);
+    /*
     for(byte i=0; i<8; i++) {
       Serial.print(addr[i], HEX);
       Serial.print(" ");
     }
+    Serial.print("\n");
+    */
+    Serial.print(dataStrings);
     Serial.print("\n");
     digitalWrite(ledpin, HIGH);
     delay(1000);
